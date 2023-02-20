67f8feb3b16176d1753a7af406dc66aabb4ee2df
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-02-20 12:23:51 +0530

    Add files via upload

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expensetrackerfullstack", "root", "Amitbhand68@", {
+const sequelize = new Sequelize("expenseproject", "root", "Amitbhand68@", {
   dialect: "mysql",
   host: "localhost",
 });
